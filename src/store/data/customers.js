5838a68eeb05bb752e85f5f73afd76dabf4e70b6
sashaadazhii <sashki.sashka@gmail.com>
2022-06-28 14:11:02 +0300

    add data[customers,vehicles], update css

--- src/store/data/customers.js
+++ src/store/data/customers.js
@@ -2,7 +2,7 @@ const customers = {
   data: [
     {
       uid: '2f61a152-9c3f-42e9-823c-e412f348ec17',
-      email: 'brett.ostrander@test.com',
+      email: 'brett.ostrander@asn.tech',
       firstName: 'Ostrander',
       lastName: 'Brett',
       userName: 'BretOstrander',
@@ -16,7 +16,7 @@ const customers = {
       firstName: 'Tony',
       lastName: 'Stark',
       userName: 't-stark',
-      cellPhones: ['497 783 4727', '044 155 7555'],
+      cellPhones: ['205 403 7504'],
       createdAt: '2022-04-02T20:50:52.667Z',
       updatedAt: '2022-05-18T10:21:21.667Z'
     },
@@ -26,36 +26,106 @@ const customers = {
       firstName: 'Jonathan',
       lastName: 'Doe',
       userName: 'j-doe',
-      cellPhones: ['497 783 4727', '044 155 7555'],
+      cellPhones: ['766 455 4909', '233 234 2211', '345 998 3409'],
       createdAt: '2022-04-02T20:50:52.667Z',
       updatedAt: '2022-05-18T10:21:21.667Z'
     },
     {
       uid: '2f61a152-9c3f-42e9-823c-e412f348ec53',
-      email: 'jonny.doe@test.com',
-      firstName: 'Jonathan',
-      lastName: 'Doe',
-      userName: 'j-doe',
-      cellPhones: ['497 783 4727', '044 155 7555'],
+      email: 'mike.smith@gmal.com',
+      firstName: 'Mike',
+      lastName: 'Smith',
+      userName: 'm-smith',
+      cellPhones: ['345 900 2340', '234 566 0987'],
       createdAt: '2022-04-02T20:50:52.667Z',
       updatedAt: '2022-05-18T10:21:21.667Z'
     },
     {
       uid: '2f61a152-9c3f-42e9-823c-e412f348ec10',
-      email: 'jonny.doe@test.com',
-      firstName: 'Jonathan',
-      lastName: 'Doe',
-      userName: 'j-doe',
+      email: 'marcus.marcus@ai.com',
+      firstName: 'Marcus',
+      lastName: 'Howard',
+      userName: 'mhovard',
       cellPhones: ['298 958 1918', '658 583 2329'],
       createdAt: '2022-04-02T20:50:52.667Z',
       updatedAt: '2022-05-18T10:21:21.667Z'
-    }
+    },
+    {
+      uid: 'ad7608eb-dfd4-40a9-876f-16972a08465f',
+      email: 'scott.sutton@example.com',
+      firstName: 'Scott',
+      lastName: 'Sutton',
+      userName: 'ssutton',
+      cellPhones: ['206 944 9639', '568 123 7640'],
+      createdAt: '2022-04-02T20:50:52.667Z',
+      updatedAt: '2022-05-18T10:21:21.667Z'
+    },
+    {
+      uid: '40b55867-ba5f-430a-a458-d936a28be20e',
+      email: 'linda.clark@mail.com',
+      firstName: 'Linda',
+      lastName: 'Clark',
+      userName: 'l-clark',
+      cellPhones: ['565 345 1234', '098 234 9076'],
+      createdAt: '2022-04-02T20:50:52.667Z',
+      updatedAt: '2022-05-18T10:21:21.667Z'
+    },
+    {
+      uid: '0721dd98-8b96-499b-9db0-74b3eaba11f1',
+      email: 'lukas.terry@mail.com',
+      firstName: 'Terry',
+      lastName: 'Lucas',
+      userName: 'tlucas',
+      cellPhones: ['440 212 0409', '161 079 9335', '095 678 3456'],
+      createdAt: '2022-04-02T20:50:52.667Z',
+      updatedAt: '2022-05-18T10:21:21.667Z'
+    },
+    {
+      uid: '26cfd801-ab35-4e96-a807-169af69d5f3e',
+      email: 'felicia.p@mail.com',
+      firstName: 'Felicia',
+      lastName: 'Perez',
+      userName: 'perez-f',
+      cellPhones: ['089 667 5002'],
+      createdAt: '2022-04-02T20:50:52.667Z',
+      updatedAt: '2022-05-18T10:21:21.667Z'
+    },
+    {
+      uid: 'e461f1c5-333c-4ed5-9b58-3b8d495d6a6f',
+      email: 'kylie.home@mail.com',
+      firstName: 'Kylie',
+      lastName: 'Holmes',
+      userName: 'kylie-home',
+      cellPhones: ['777 031 2059', '558 665 6132'],
+      createdAt: '2022-04-02T20:50:52.667Z',
+      updatedAt: '2022-05-18T10:21:21.667Z'
+    },
+    {
+      uid: '5a2cec0a-669f-4191-a761-aa734f6559e6',
+      email: 'mich.adam@some.ca',
+      firstName: 'Michel',
+      lastName: 'Adams',
+      userName: 'michel-ada',
+      cellPhones: ['633 665 0752', '5998 342 2158', '096 429 6484'],
+      createdAt: '2022-04-02T20:50:52.667Z',
+      updatedAt: '2022-05-18T10:21:21.667Z'
+    },
+    {
+      uid: 'e2bf591a-f346-462e-b9de-b27d336d6d43',
+      email: 'krin.brooks@example.com',
+      firstName: 'Krin',
+      lastName: 'Brooks',
+      userName: 'Brooks',
+      cellPhones: ['549 730 0464'],
+      createdAt: '2022-04-02T20:50:52.667Z',
+      updatedAt: '2022-05-18T10:21:21.667Z'
+    },
   ],
   pagination: {
     page: 1,
     perPage: 20,
-    total: 2
+    total: 12
   }
 }
 
-export {customers}
+export { customers }
