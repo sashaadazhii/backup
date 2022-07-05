4332907c3bf54c0ede812d3cd1adae25e8e09c4f
tuuz1 <tuuz121@gmail.com>
2022-07-05 17:45:24 +0300

    vehicle health / add modal

--- src/store/data/cards.js
+++ src/store/data/cards.js
@@ -3,7 +3,7 @@ const cardsList = [
     uid: '33d68e8e-34cf-42d8-837a-fa7914acd8ce',
     status: 'No Status',
     name: 'Cabin Filter',
-    approvalStatus: 'No status',
+    approvalStatus: 'Approved By Service Advisor',
     service: '48,000 KM / 24 Month',
     progress: 50,
     select: false
@@ -12,34 +12,34 @@ const cardsList = [
     uid: '3a069904-033f-46ba-b7b8-95950d7655b3',
     status: 'No Status',
     name: 'Cooling System Service',
-    approvalStatus: 'No status',
+    approvalStatus: 'Approved By Customer',
     service: '48,000 KM / 24 Month',
     progress: 50,
     select: false
   },
   {
     uid: '1bca8f37-1057-4972-9e28-481ea4ffaff8',
-    status: 'No Status',
+    status: 'Good',
     name: 'Transmission Service',
-    approvalStatus: 'No status',
+    approvalStatus: 'Approved By Service Advisor',
     service: '48,000 KM / 24 Month',
     progress: 40,
     select: false
   },
   {
     uid: 'cef6751c-5b73-462f-8347-3dff591fb4c8',
-    status: 'No Status',
+    status: 'Recommended',
     name: 'Power Steering System Service',
-    approvalStatus: 'No status',
+    approvalStatus: 'Temporary Declined',
     service: '48,000 KM / 24 Month',
     progress: 50,
     select: false
   },
   {
     uid: '3b0e5a07-1714-40ee-bcb5-11bdd627f080',
-    status: 'No Status',
+    status: 'Component Unsafe',
     name: 'Engine Tuneup/Spark Plugs',
-    approvalStatus: 'No status',
+    approvalStatus: 'Permanently Declined',
     service: '48,000 KM / 24 Month',
     progress: 50,
     select: false
@@ -48,7 +48,7 @@ const cardsList = [
     uid: 'b6c2bfde-63bc-485f-adf9-4e2ce70d5145',
     status: 'No Status',
     name: 'Engine Timing Belt',
-    approvalStatus: 'No status',
+    approvalStatus: 'Approved For Next Visit',
     service: '48,000 KM / 24 Month',
     progress: 50,
     select: false
@@ -57,7 +57,7 @@ const cardsList = [
     uid: '46436359-2119-4879-bdc8-3836890ed17a',
     status: 'No Status',
     name: 'Direct Injection System Service',
-    approvalStatus: 'No status',
+    approvalStatus: 'No Status',
     service: '48,000 KM / 24 Month',
     progress: 50,
     select: false
@@ -66,7 +66,7 @@ const cardsList = [
     uid: 'f13e6a2e-97e0-4955-b299-a7d2320dacf5',
     status: 'No Status',
     name: 'Brake Fluid System Service',
-    approvalStatus: 'No status',
+    approvalStatus: 'No Status',
     service: '48,000 KM / 24 Month',
     progress: 50,
     select: false
--- src/views/WorkOrder/VehicleHealth/Slot/index.vue
+++ src/views/WorkOrder/VehicleHealth/Slot/index.vue
@@ -1,9 +1,9 @@
 <template>
   <div class="card__wrapper">
     <div class="y-check" :class="{'-active': card.select}" @click="select(card.uid)" />
-    <Label :label="card.status" size="small" border class="card__label" />
+    <Label :label="card.status" size="small" class="card__label" :class="labelClass(card.status)" />
     <div class="card__title">{{ card.name }}</div>
-    <Label :label="card.approvalStatus" size="small" icon="i-rp_done" border circle class="card__label" />
+    <Label :label="card.approvalStatus" size="small" icon="i-rp_done" circle class="card__label -shadow" :class="labelClass(card.approvalStatus)" iconSize="8px" />
     <div class="card__progress">
       <div class="card__progress-title">{{ card.progress }}%</div>
       <div class="card__progress-bar"><span :style="{width: card.progress + '%'}" /></div>
@@ -14,7 +14,6 @@
 </template>
 
 <script>
-// import Request from './RequestComponent'
 import Menu from '@/components/Yaro/Menu'
 import Label from '@/components/Yaro/Label'
 import {mapMutations} from 'vuex'
@@ -46,7 +45,20 @@ export default {
   methods: {
     ...mapMutations({
       select: 'company/cards/select'
-    })
+    }),
+    labelClass(status) {
+      return {
+        '-orange': status === 'Recommended',
+        '-red': status === 'Component Unsafe',
+        '-bluegreen': status === 'Canned Service Completed',
+        '-none': status === 'No Status',
+        '-green': status === 'Approved By Service Advisor',
+        '-green -border': status === 'Approved By Customer',
+        '-bluegreen': status === 'Temporary Declined',
+        '-red': status === 'Permanently Declined',
+        '-purple': status === 'Approved For Next Visit'
+      }
+    }
   }
 }
 </script>
