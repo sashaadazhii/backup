f6a0dfc5c03b88990c3440908bd682c040b1f415
sashaadazhii <sashki.sashka@gmail.com>
2022-07-05 15:05:33 +0300

    update pages css

--- src/views/Settings/Statuses/index.vue
+++ src/views/Settings/Statuses/index.vue
@@ -1,11 +1,13 @@
 <template>
   <div class="statuses__wrapper">
-    <div class="statuses__header">
-      <div class="statuses__header-title">Statuses</div>
-      <div class="statuses__header-subtitle">Add as many statuses to each of the major phases of a work order.</div>
-    </div>
-    <div class="statuses__main">
-      <Status v-for="status of logicalStatuses" :key="status.uid" :status="status" />
+    <div class="statuses__container">
+      <div class="statuses__header">
+        <div class="statuses__header-title">Statuses</div>
+        <div class="statuses__header-subtitle">Add as many statuses to each of the major phases of a work order.</div>
+      </div>
+      <div class="statuses__main">
+        <Status v-for="status of logicalStatuses" :key="status.uid" :status="status" />
+      </div>
     </div>
   </div>
 </template>
