22586526a36899d86a5da367d695e0d374b1c89d
tuuz1 <tuuz121@gmail.com>
2022-07-18 08:24:19 +0300

    vehicle-health / card notes

--- src/views/WorkOrder/VehicleHealth/CardPage/Notes/index.vue
+++ src/views/WorkOrder/VehicleHealth/CardPage/Notes/index.vue
@@ -1,5 +1,40 @@
 <template>
-  <div class="block__wrapper"></div>
+  <div class="block__wrapper">
+    <div class="block__header">
+      <div class="block__title">Notes</div>
+      <button class="block__btn"><i class="i-add_circle" /><span>Add New Note</span></button>
+    </div>
+    <div class="block__body">
+      <div class="note__wrapper -bluegreen">
+        <div class="note__line" />
+        <div class="note__inner">
+          <div class="note__header">Lorem ipsum dolor sit amet</div>
+          <div class="note__body">
+            Praesent et dolor non justo commodo ultrices. Quisque laoreet ante non neque aliquet, sit amet hendrerit tellus tristique. Nulla fermentum ...
+          </div>
+          <div class="note__footer">
+            <Label alias="MB" circle size="small" class="-dark" />
+            <span>Maynard Bauman</span>
+            <span>at 22 Sep 2019 </span>
+          </div>
+        </div>
+      </div>
+      <div class="note__wrapper -orange">
+        <div class="note__line" />
+        <div class="note__inner">
+          <div class="note__header">Lorem ipsum dolor sit amet</div>
+          <div class="note__body">
+            Praesent et dolor non justo commodo ultrices. Quisque laoreet ante non neque aliquet, sit amet hendrerit tellus tristique. Nulla fermentum ...
+          </div>
+          <div class="note__footer">
+            <Label alias="MB" circle size="small" class="-dark" />
+            <span>Maynard Bauman</span>
+            <span>at 22 Sep 2019 </span>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
@@ -9,7 +44,7 @@ import Menu from '@/components/Yaro/Menu'
 
 export default {
   name: 'CardPageNotes',
-  // components: {Button, Label},
+  components: {Label},
   data() {
     return {}
   }
