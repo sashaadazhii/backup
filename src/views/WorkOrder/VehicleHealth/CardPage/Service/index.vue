67ef84e7d2f058c83d9cf011678f3b1217c88d60
tuuz1 <tuuz121@gmail.com>
2022-07-18 08:37:15 +0300

    vehicle-health / card warranty

--- src/views/WorkOrder/VehicleHealth/CardPage/Service/index.vue
+++ src/views/WorkOrder/VehicleHealth/CardPage/Service/index.vue
@@ -1,15 +1,29 @@
 <template>
-  <div class="block__wrapper"></div>
+  <div class="block__wrapper">
+    <div class="warranty__wrapper">
+      <div class="warranty__header">Lights</div>
+      <div class="warranty__body">
+        Freeup, clean and lube sliders. Clean rust from brake rotors. This is done to extend the life of the brakes and extend your brake warranty to 3
+        years/60000km)
+      </div>
+      <div class="warranty__progress red"><span style="width: 90%" /></div>
+      <div class="warranty__time">3 Years / 60,000KM</div>
+    </div>
+    <div class="warranty__wrapper">
+      <div class="warranty__header">Front Brake Service</div>
+      <div class="warranty__body">
+        Freeup, clean and lube sliders. Clean rust from brake rotors. This is done to extend the life of the brakes and extend your brake warranty to 3
+        years/60000km)
+      </div>
+      <div class="warranty__progress green"><span style="width: 40%" /></div>
+      <div class="warranty__time">3 Years / 60,000KM</div>
+    </div>
+  </div>
 </template>
 
 <script>
-import Button from '@/components/Yaro/Button'
-import Label from '@/components/Yaro/Label'
-import Menu from '@/components/Yaro/Menu'
-
 export default {
   name: 'CardPageService',
-  // components: {Button, Label},
   data() {
     return {}
   }
