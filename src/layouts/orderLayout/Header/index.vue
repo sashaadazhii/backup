fe9e270f3cfd33dcb0b52e68b6af2089ad7cad0d
tuuz1 <tuuz121@gmail.com>
2022-09-12 18:41:15 +0300

    update work order header

--- src/layouts/orderLayout/Header/index.vue
+++ src/layouts/orderLayout/Header/index.vue
@@ -17,7 +17,7 @@
       <Label v-if="order.partsOrdered" class="header__label" icon="i-build" border iconColor="#6B7280" iconSize="18px" size="large" />
     </div>
     <div class="header__nav">
-      <router-link v-if="uid !== 'new' && uid !== 'sa-flow' && uid !== 'sa-billing'" class="header__nav-link" :to="`/work-order/${uid}/general`">Settings</router-link>
+      <router-link v-if="uid === 'new' && uid === 'sa-flow' && uid === 'sa-billing'" class="header__nav-link" :to="`/work-order/${uid}/general`">Settings</router-link>
       <router-link class="header__nav-link" :to="`/work-order/${uid}/vehicle-health`">Vehicle Health</router-link>
       <router-link class="header__nav-link" :to="`/work-order/${uid}/vehicle-info`">Vehicle Info</router-link>
       <router-link v-if="uid !== 'new' && uid !== 'tech-flow' && uid !== 'tech-start'" class="header__nav-link" :to="`/work-order/${uid}/finance`"
