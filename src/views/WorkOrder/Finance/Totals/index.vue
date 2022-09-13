bfa2eee9601d4a398c3e52f4758f6f4a20791f3e
tuuz1 <tuuz121@gmail.com>
2022-09-13 16:00:22 +0300

    update finance main page

---
+++ src/views/WorkOrder/Finance/Totals/index.vue
@@ -0,0 +1,161 @@
+<template>
+  <div class="totals__wrapper">
+    <div class="totals__title">Totals</div>
+    <Total v-for="total of totals" :key="total.id" :total="total" />
+    
+    <!-- <div class="totals__block block">
+      <div class="block__row">
+        <div class="block__icon -grey" />
+        <div class="block__name">Total Parts</div>
+        <div class="block__desc">$249</div>
+      </div>
+      <div class="block__row">
+        <div class="block__icon -grey" />
+        <div class="block__name">Total Labour</div>
+        <div class="block__desc">$220</div>
+      </div>
+      <div class="block__row">
+        <div class="block__icon -grey" />
+        <div class="block__name">Sub Total</div>
+        <div class="block__desc">$550</div>
+      </div>
+      <div class="block__row">
+        <div class="block__icon -grey" />
+        <div class="block__name">HST</div>
+        <div class="block__desc">$71.5</div>
+      </div>
+      <div class="block__row">
+        <div class="block__icon -dark" />
+        <div class="block__name">Grand Total</div>
+        <div class="block__desc">$656</div>
+      </div>
+    </div> -->
+  </div>
+</template>
+
+<script>
+import Total from './Total'
+
+export default {
+  name: 'FinanceTotals',
+  components: {Total},
+  data() {
+    return {
+      totals: [
+        {
+          id: 1,
+          color: 'green',
+          name: 'Cooling System Service',
+          price: 320,
+          parts: [
+            {
+              id: 1,
+              name: 'Part #2323-342-234',
+              quantity: 12,
+              price: 12
+            },
+            {
+              id: 2,
+              name: 'Part #2323-342-124',
+              quantity: 8,
+              price: 4
+            },
+            {
+              id: 3,
+              name: 'Part #5234-342-234',
+              quantity: 2,
+              price: 22
+            },
+            {
+              id: 4,
+              name: 'Environmental Charge',
+              quantity: 0,
+              price: 180
+            },
+            {
+              id: 5,
+              name: 'Engine Belt',
+              quantity: 0,
+              price: 230
+            }
+          ]
+        },
+        {
+          id: 1,
+          color: 'green',
+          name: 'Air filter change',
+          price: 40,
+          parts: [
+            {
+              id: 1,
+              name: 'Air Filter',
+              quantity: 0,
+              price: 22
+            },
+            {
+              id: 2,
+              name: 'Part #2323-342-124',
+              quantity: 4,
+              price: 4
+            }
+          ]
+        },
+        {
+          id: 3,
+          color: 'red',
+          name: 'Cabin Filter change',
+          price: 48,
+          parts: [
+            {
+              id: 1,
+              name: 'Cabin Filter',
+              quantity: 0,
+              price: 30
+            },
+            {
+              id: 2,
+              name: 'Part #2323-342-124',
+              quantity: 4,
+              price: 4
+            }
+          ]
+        },
+        {
+          id: 4,
+          color: 'grey',
+          name: 'Total Parts',
+          price: 248
+        },
+        {
+          id: 5,
+          color: 'grey',
+          name: 'Total Labour',
+          price: 220
+        },
+        {
+          id: 6,
+          color: 'grey',
+          name: 'Sub Total',
+          price: 580
+        },
+        {
+          id: 7,
+          color: 'grey',
+          name: 'HST',
+          price: 98
+        },
+        {
+          id: 7,
+          color: 'dark',
+          name: 'Grand Total',
+          price: 848
+        }
+      ]
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+@import 'style';
+</style>
--- src/views/WorkOrder/Finance/index.vue
+++ src/views/WorkOrder/Finance/index.vue
@@ -1,18 +1,65 @@
 <template>
   <div class="finance__wrapper">
-    <div class="finance__cards"><Card /> <Request /></div>
-    <div class="finance__info"><Chart /><Total /></div>
+    <div class="finance__header">
+      <div class="finance__title">Quote # 121213</div>
+      <Label label="In progress" size="small" class="-orange" />
+    </div>
+    <div class="finance__inner">
+      <div class="finance__cards">
+        <Card v-for="(card, idx) of cards" :key="idx" :card="card" @onCardDone="card.done = true" />
+        <Request />
+      </div>
+      <div class="finance__info"><Totals /></div>
+    </div>
   </div>
 </template>
 
 <script>
 import Card from './Card'
 import Request from './Request'
-import Chart from './Chart'
-import Total from './Total'
+import Totals from './Totals'
+import Label from '@/components/Yaro/Label'
+
 export default {
   name: 'FinancePage',
-  components: {Card, Chart, Total, Request}
+  /* eslint-disable  */
+  components: {Card, Totals, Request, Label},
+
+  data() {
+    return {
+      cards: [
+        {
+          id: 1,
+          status: 'Recommended',
+          approvalStatus: 'Approved For Next Visit',
+          title: 'Cooling System Service',
+          price: 320,
+          description:
+            'Complete engine Tune-up and induction system service. Recommended to improve fuel mileage, emissions, prevent misfires and improve overall engine performance',
+          isDone: false
+        },
+        {
+          id: 2,
+          status: 'Recommended',
+          approvalStatus: 'Approved By SA',
+          title: 'Air filter change',
+          price: 380,
+          description:
+            'Complete engine Tune-up and induction system service. Recommended to improve fuel mileage, emissions, prevent misfires and improve overall engine performance',
+          isDone: false
+        },
+        {
+          id: 4,
+          status: 'Component Unsafe',
+          approvalStatus: 'Permanently Declined',
+          title: 'Engine Belt',
+          price: 200,
+          description:
+            'Complete engine Tune-up and induction system service. Recommended to improve fuel mileage, emissions, prevent misfires and improve overall engine performance'
+        }
+      ]
+    }
+  }
 }
 </script>
 
