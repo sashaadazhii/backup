d93b23f750b8d88909954c334494e8dca5b5ad87
sashaadazhii <sashki.sashka@gmail.com>
2022-11-02 21:17:54 +0200

    add customer modals

---
+++ src/views/WorkOrder/CustomerView/AskingApproveModal/index.vue
@@ -0,0 +1,65 @@
+<template>
+  <vue-final-modal @before-open="beforeOpen">
+    <!-- @before-open="beforeOpen" -->
+    <div class="modal">
+      <div class="modal__first">
+        <div class="modal__icon"><i class="i-circle_close" /></div>
+        <div class="modal__title">Do you want to approve order?</div>
+        <div class="modal__buttons">
+          <Button label="Proceed Payment" @click="approve" />
+          <Button label="Cancel" border @click="$vfm.hide('AskingApproveModal')" />
+        </div>
+      </div>
+    </div>
+  </vue-final-modal>
+</template>
+
+<script>
+import {mapState, mapActions} from 'vuex'
+import Button from '@/components/Yaro/Button'
+// import StrapiModal from './StrapiModal'
+export default {
+  name: 'AskingApproveModal',
+  components: {Button},
+  data() {
+    return {
+      id: null
+    }
+  },
+  computed: {
+    ...mapState({
+      card: s => s.cards.card,
+      cards: s => s.cards.cards
+    })
+  },
+  methods: {
+    ...mapActions({
+      fetch: 'cards/fetch',
+      findCard: 'cards/find',
+      update: 'cards/updateCard'
+    }),
+    async approve() {
+      // await this.update({id: this.card.id, workOrderID: this.card.workOrderID, params: {status: 'Done', approvedWith: 'Approved by You'}})
+      this.$vfm.hide('AskingApproveModal')
+      this.$vfm.show(
+        {
+          component: StrapiModal,
+          bind: {
+            name: 'StrapiModal'
+          }
+        },
+        this.card.id
+      )
+    },
+    async beforeOpen(e) {
+      this.id = e.ref.params._value
+      await this.findCard(this.id)
+      await this.fetch(this.card.workOrderID)
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+@import 'style';
+</style>
---
+++ src/views/WorkOrder/CustomerView/CardPage/Part.vue
@@ -0,0 +1,32 @@
+<template>
+  <div v-for="part in parts" :key="part.id" class="list__item" :class="{hide: card.partsForCustomer === 'Display Total Price Only'}">
+    <div v-if="card.partsForCustomer === 'Display Total Price & Parts'" class="list__inner wide">
+      <div class="list__icon">
+        <i v-if="part.isService || part.isLabour" class="i-monetization_on" :style="[part.isService ? 'color: #BA8AE7' : 'color: #2C9AFF']" />
+        <i v-else class="i-build" />
+      </div>
+      <div class="list__info">
+        <div class="list__desc">{{ part.description }}</div>
+      </div>
+    </div>
+
+    <div v-else-if="card.partsForCustomer === 'Display Total Price Only'" class="list__inner"></div>
+
+    <div v-else-if="card.partsForCustomer === 'Display Parts & Pricing'" class="list__inner">
+      <div class="list__icon">
+        <i v-if="part.isService || part.isLabour" class="i-monetization_on" :style="[part.isService ? 'color: #BA8AE7' : 'color: #2C9AFF']" />
+        <i v-else class="i-build" />
+      </div>
+      <div class="list__info">
+        <div class="list__desc">{{ part.description }}</div>
+      </div>
+      <div class="list__text amount">{{ formatter(part.price) }}</div>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  name: 'PartCustomerView',
+  props: {part: {type: Object}}
+}
+</script>
